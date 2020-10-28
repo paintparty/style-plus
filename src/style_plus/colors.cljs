0c84cf9964ef4c2458b4125ac5f1019ba4c0c342
Jeremiah Coyle <jcoyle@gmail.com>
2020-10-28 13:00:31 -0700

    Add colors ns

---
+++ src/style_plus/colors.cljs
@@ -0,0 +1,42 @@
+(ns style-plus.colors)
+
+(def white :#FFFFFF)
+(def whitesmoke :#EFEFEF)
+(def silver :#DDDDDD)
+(def gray :#AAAAAA)
+(def grey :#AAAAAA)
+(def black :#111111)
+(def navy :#001F3F)
+(def blue :#0074D9)
+(def aqua :#7FDBFF)
+(def teal :#39CCCC)
+(def olive :#3D9970)
+(def green :#2ECC40)
+(def lime :#01FF70)
+(def yellow :#FFDC00)
+(def orange :#FF851B)
+(def red :#FF4136)
+(def fuchsia :#F012BE)
+(def purple :#B10DC9)
+(def maroon :#85144B)
+
+(defonce colors+
+  {:white white
+   :whitesmoke whitesmoke
+   :silver silver
+   :gray gray
+   :grey grey
+   :black black
+   :navy navy
+   :blue blue
+   :aqua aqua
+   :teal teal
+   :olive olive
+   :green green
+   :lime lime
+   :yellow yellow
+   :orange orange
+   :red red
+   :fuchsia fuchsia
+   :purple purple
+   :maroon maroon})
